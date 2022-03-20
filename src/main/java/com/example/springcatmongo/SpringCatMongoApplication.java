72dd2b98a519a2fd41c3b6f3a623c55c9bde714e
hiumei <hiu2mei4@gmail.com>
2022-03-20 15:31:17 -0400

    init commit

---
+++ src/main/java/com/example/springcatmongo/SpringCatMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcatmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCatMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCatMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcatmongo/SpringCatMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcatmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCatMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
